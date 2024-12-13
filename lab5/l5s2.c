2b223f9eb6ccf7550d7d9f25e7548565b425e2ca
Lydia Myla <97082747+lydsjm@users.noreply.github.com>
2024-12-12 23:15:57 -0800

    Add files via upload

---
+++ lab5/l5s2.c
@@ -0,0 +1,40 @@
+/*
+Name : Lydia Myla
+Date : 10/21 - 2:15
+Title: Lab 5 - Step 2
+Description: modify code in step 1 for using mutex locks 
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <fcntl.h>
+
+#define NTHREADS 10
+pthread_t threads[NTHREADS];
+//sem_t *mutex;
+pthread_mutex_t mutex;
+
+void* go(void* arg) {
+  pthread_mutex_lock(&mutex);
+  printf("Thread %d Entered Critical Section..\n", (int)arg); //critical section
+  sleep(1);
+  pthread_mutex_unlock(&mutex); //exit section
+  return (NULL);
+}
+
+int main() {
+  //mutex = sem_open("mutex", O_CREAT, 0644, 1);
+  pthread_mutex_init(&mutex, NULL);
+  static int i;
+  for (i = 0; i < NTHREADS; i++)
+    pthread_create(&threads[i], NULL, go, (void *)(size_t)i);
+  for (i = 0; i < NTHREADS; i++) {
+    pthread_join(threads[i],NULL);
+    printf("\t\t\tThread %d returned \n", i);
+  }
+  printf("Main thread done.\n");
+  pthread_mutex_destroy(&mutex);
+  return 0;
+}
