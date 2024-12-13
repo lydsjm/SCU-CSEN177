c2064fb7250f9065f67d2e0b1f21f9026223e352
Lydia Myla <97082747+lydsjm@users.noreply.github.com>
2024-12-12 23:21:30 -0800

    Delete createfiles.sh

--- createfiles.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cat /dev/urandom | head -c 100000 > file1.bin
-cat /dev/urandom | head -c 1000000 > file2.bin
-cat /dev/urandom | head -c 10000000 > file3.bin
-cat /dev/urandom | head -c 100000000 > file4.bin
